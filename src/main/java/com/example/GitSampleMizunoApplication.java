d9d3f66c6134c59d7acd9490a0becf6a6146a017
mizunotomoya <tomoya.mizuno@rakus-parters.co.jp>
2021-11-04 11:59:44 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleMizunoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleMizunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleMizunoApplication.class, args);
+	}
+
+}
